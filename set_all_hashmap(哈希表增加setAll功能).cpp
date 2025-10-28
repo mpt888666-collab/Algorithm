7410db9f164887df42f4787b0dc09d3c6ced5168
mpt888666-collab <mpt888666@gmail.com>
2025-10-28 09:20:14 +0800

    Update set_all_hashmap(哈希表增加setAll功能).cpp

--- set_all_hashmap(哈希表增加setAll功能).cpp
+++ set_all_hashmap(哈希表增加setAll功能).cpp
@@ -1,26 +1,38 @@
 //set_all功能的hash表，原有基础上增加了时间戳功能，set_all_time之前的的所有value都变成了set_all_value
 
+template<typename Key, typename Value>
 class My_Unordered_Map {
 public:
-    My_Unordered_Map() : _set_all_value(0), _set_all_time(-1), _cnt(0){}
+    My_Unordered_Map() : _set_all_value(Value{}), _set_all_time(-1), _cnt(0){}
 
-    void put(const int &key, const int &value) {
+    void put(const Key &key, const Value &value) {
         _map[key] = std::make_pair(value, _cnt++);
     }
 
-    void set_all(const int &value) {
+    void set_all(const Value &value) {
         _set_all_time = _cnt++;
         _set_all_value = value;
     }
 
-    int get(const int &key) {
-        if (!_map.contains(key)) return std::numeric_limits<int>::min();
-        if (_map[key].second < _set_all_time) return _set_all_value;
-        else return _map[key].first;
+    Value get(const Key &key) {
+        auto it = _map.find(key);
+        if (it == _map.end()) {
+            if constexpr (std::numeric_limits<Value>::is_specialized)
+                return std::numeric_limits<Value>::lowest();
+            else
+                return Value{};
+        }
+        if (it->second.second < _set_all_time)
+            return _set_all_value;
+        else
+            return it->second.first;
     }
 private:
-    std::unordered_map<int, std::pair<int, int>> _map;
-    int _set_all_value;
+    std::unordered_map<Key, std::pair<Value, int>> _map;
+    Value _set_all_value;
     int _set_all_time;
     int _cnt;
 };
+
+
+
